5461883f3ed2776009ede70bf81da004716b6978
Thabata356 <thabatav7@gmail.com>
2025-09-22 08:58:43 -0300

    :sparkles: Feat: Commitar prova prática.

---
+++ AppRpgEtec/AppRpgEtec/Services/Enderecos/EnderecoService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.Services.Enderecos
+{
+    public class EnderecoService : Request
+    {
+        //Endereco endereco = new Endereco();
+        EnderecoService enderecoService = new EnderecoService();
+        private readonly Request _request;
+        private const string apiUrlBase = "https://rpgapi3ai2025.azurewebsites.net/Enderecos";
+        //Azure: https://rpgapi3ai2025.azurewebsites.net/Enderecos
+        //Somee: http://luizfernando.somee.com/RpgApi/Enderecos
+
+        public EnderecoService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Endereco> GetCepAsync(int cepId)
+        {
+            string urlComplementar = string.Format("/{0}", cepId);
+            string cep = await _request.GetAsync<Models.Endereco>(apiUrlBase + urlComplementar);
+            return cep;
+        }
+    }
+}
